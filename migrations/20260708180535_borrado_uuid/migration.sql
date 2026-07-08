92d628300c6a908c4d2f3dc681aa116cc72c7b51
David Santamaría Rogado <howl.nsp@gmail.com>
2026-07-08 22:29:14 +0200

    models: adjust field types and map names camelCase in code snake_case in db
    Signed-off-by: David Santamaría Rogado <howl.nsp@gmail.com>

--- migrations/20260708180535_borrado_uuid/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE "eventos" DROP CONSTRAINT "eventos_presupuestoId_fkey";
-
--- AlterTable
-ALTER TABLE "eventos" ALTER COLUMN "presupuestoId" SET DATA TYPE TEXT;
-
--- AddForeignKey
-ALTER TABLE "eventos" ADD CONSTRAINT "eventos_presupuestoId_fkey" FOREIGN KEY ("presupuestoId") REFERENCES "presupuestos"("id_presupuesto") ON DELETE SET NULL ON UPDATE CASCADE;
